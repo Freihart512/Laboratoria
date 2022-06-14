883bbc197e3c9e13c9a0830e9f580a6f926f7c65
CinthiaVB <cinthiaveb@gmail.com>
2022-06-14 17:56:46 -0500

    firebase Auth

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { TakeOrdersComponent } from './take-orders/take-orders.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +24,16 @@ import { TakeOrdersComponent } from './take-orders/take-orders.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/log-in/log-in.component.ts
+++ my-app/src/app/log-in/log-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  loginUser: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb:FormBuilder,
+    private afAuth: AngularFireAuth,
+    private router: Router) {
+    this.loginUser = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+   }
 
   ngOnInit(): void {
   }
