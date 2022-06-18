94d012a8500fc0b5c5f1e03dda1968b6a94015e6
CinthiaVB <cinthiaveb@gmail.com>
2022-06-18 09:29:32 -0500

    view waiter header and lateral menu

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { TakeOrdersComponent } from './components/take-orders/take-orders.component';
@@ -14,11 +14,8 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideFunctions,getFunctions } from '@angular/fire/functions';
 import { provideStorage,getStorage } from '@angular/fire/storage';
-<<<<<<< HEAD
-import { ReactiveFormsModule } from '@angular/forms';
-=======
 import { ChefViewComponent } from './components/chef-view/chef-view.component';
->>>>>>> cf509547301cccb3a7a1f9d36bef5a8969b5db8c
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -30,15 +27,14 @@ import { ChefViewComponent } from './components/chef-view/chef-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
